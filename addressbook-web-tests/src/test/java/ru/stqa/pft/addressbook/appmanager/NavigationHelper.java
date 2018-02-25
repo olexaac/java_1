739a8bda2ce9715d52c3599d0a43c7ac5c871870
olexaac <arbeidnsk@gmail.com>
2018-02-25 22:02:19 +0700

    Выделены классы-помощники для тестов создания и удаления групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Админ on 25.02.2018.
+ */
+public class NavigationHelper {
+  private FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+}
