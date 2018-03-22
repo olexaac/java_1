4f7fc7092a33f96baf517f43be3bfeaa164d3107
olexaac <arbeidnsk@gmail.com>
2018-03-22 23:13:57 +0700

    Выполнена загрузка тестовых данных из файла для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.omg.CORBA.Object;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,11 +16,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<java.lang.Object[]> validGroups() {
+  public Iterator<java.lang.Object[]> validGroups() throws IOException {
     List<java.lang.Object[]> list = new ArrayList<java.lang.Object[]>();
-    list.add(new java.lang.Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new java.lang.Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new java.lang.Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
